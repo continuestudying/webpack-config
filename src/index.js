eb2a749e30e1a78879a3956d788118d71c63dc4e
continuestudying <continuestudying@gmail.com>
2022-10-23 13:36:39 +0700

    Add css, scss, postcss, browserslist to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import ('/styles/index.scss');
+
 const testData = {
   name: "Webpack projet",
   version: "1",
   description: "This is the first demo for webpack"
 }
 
-console.log("The first output: " + testData);
+console.log(testData);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
+  target = "browserslist";
 }
 
-module.exports = { 
+module.exports = {
   mode: mode,
+  target: target,
   module: {
     rules: [
+      {
+        test: /\.(s[ac]|c)ss/i,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
-
+  plugins: [new MiniCSSExtractPlugin()],
   devtool: "source-map",
   devServer: {
     static: "./dist",
